8ef7bc26efcf358dfd44c16648c03f3c788d7e1a
impulse <74218519+crash-the-mode@users.noreply.github.com>
2021-04-27 22:29:15 -0400

    drew data

--- script.js
+++ script.js
@@ -15,9 +15,11 @@ async function drawHeatMap() {
 	const yAccessor = d => d.month;
 //	console.log(yAccessor(mVar[200]), typeof yAccessor(mVar[200]));
 	
+	const varAccessor = d => d.variance;
+	
 	// 2. Create dimensions
 	
-	const width = 1200;
+	const width = 1500;
 	const height = 900;
 
 	let dimensions = {
@@ -55,18 +57,50 @@ async function drawHeatMap() {
 	console.log(xdomain);
 	const xScale = d3.scaleBand()
 		.domain([...xdomain])
-		.range([0, dimensions.boundedWidth]);
+		.range([0, dimensions.boundedWidth])
+		.paddingInner(0);
 
+	const ymm = d3.extent(mVar, yAccessor);
+//	console.log(ymm);
+	const ydomain = [];
+	for( let i = ymm[0]; i <= ymm[1]; i++ )
+	{
+		ydomain.push(i);
+	}
+	
 	const yScale = d3.scaleBand()
-		.domain
+		.domain([...ydomain])
+		.range([0, dimensions.boundedHeight])
+		.paddingInner(0);
+
+//	console.log(d3.extent(mVar, varAccessor));
+	const colorScale = d3.scaleThreshold()
+		.domain([-5, -3, -1, 1, 3, 5])
+		.range(["#f7f7f7","#d1e5f0","#67a9cf","#2166ac", "#b2182b","#ef8a62","#fddbc7"]);
 
 	// 5. Draw data
 	
+	const barWidth = dimensions.boundedWidth / xdomain.length;
+	const barHeight = dimensions.boundedHeight / ydomain.length;
 	graph.selectAll("rect")
 		.data(mVar)
 		.enter()
 		.append("rect")
 		.attr("x", d => xScale(xAccessor(d)))
+		.attr("y", d => yScale(yAccessor(d)))
+		.attr("height", barHeight)
+		.attr("width", barWidth)
+		.attr("fill", d => colorScale(varAccessor(d)))
+
+	// 6. Draw peripherals
+	
+	const xAxisGenerator = d3.axisBottom()
+		.scale(xScale);
+
+	const xAxis = graph.append("g")
+		.call(xAxisGenerator)
+		.style("transform", `translateY(${dimensions.boundedHeight}px)`);
+
 }
 
 drawHeatMap();
